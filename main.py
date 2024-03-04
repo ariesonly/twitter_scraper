83be7eb59dd1836b15e7a21e1878ccc7ec92cd30
Muhammad Ishtiaq <ariesonly@gmail.com>
2024-03-04 17:48:22 +0500

    tweepy added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello')
+import tweepy
+
+auth = tweepy.OAuthHandler("YLG67nLGZeGtVshL0AxuM23VK", "AF9caoD2orVEctqK9DtwBOfAV38x8xWWPLT47maCQEtKXni2VU")
+auth.set_access_token("96074027-nK3nGK6nCeHrlewnxkkQdbMLMcwzR3rWoMvMK1Yw7", "muqrcTqarEu8S8z2JS4ztBGU0b601a9eyrQiqUYdGbCRy")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    tweets = api.user_timeline(screen_name='ariesonly', count=2)
+    # Assuming you have retrieved the tweets and stored them in the 'tweets' variable
+    for tweet in tweets:
+        print(tweet.text)
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
